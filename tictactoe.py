5f3c65949b3f5fd299d1f9b9a4a4857c53125764
Andrew Elkouri <andrewelkouri@gmail.com>
2020-05-23 16:40:02 -0400

    add simple validation

--- tictactoe.py
+++ tictactoe.py
@@ -17,19 +17,34 @@ def render(board):
 
     print(' ', '-' * border_width)
 
-def get_move():
-    # TODO write separate validator
-    x = input('=> What is your move\'s X co-ordinate? ')
-    y = input('=> What is your move\'s Y co-ordinate? ')
+def get_valid_coordinate(coord_str):
+    while True:
+        coord = input('=> What is your move\'s {} coordinate? '.format(coord_str))
+        if not coord.isdigit() or int(coord) < 0 or int(coord) >= 3:
+            print('Invalid coordinate. Please enter a number between 0 and 2.')
+        else:
+            return int(coord)
+
+def get_valid_move(board):
+    x, y = get_valid_coordinate('X'), get_valid_coordinate('Y')
+    while board[x][y] != None:
+        print('Sorry, ({}, {}) is already taken. Please try again.'.format(x, y))
+        x, y = get_valid_coordinate('X'), get_valid_coordinate('Y')
     return (x, y)
+
+def make_move(board, move, player):
+    updated_board = board.copy()
+    x, y = move
+    updated_board[x][y] = player
+    return updated_board
 
 if __name__ == '__main__':
     board = new_board()
     board[0][1] = 'X'
     board[2][1] = 'O'
     board[0][0] = 'O'
-    render(board)
 
-    x, y = get_move()
-    print("x =", x)
-    print("y =", y)
+    move = get_valid_move(board)
+    render(board)
+    b = make_move(board, move, 'O')
+    render(b)
